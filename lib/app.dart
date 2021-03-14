86b2cafe1d01c11fbc96a282d6efb00e15a78df3
Jesús Otero <jesusoterogomez@gmail.com>
2021-03-14 22:08:45 +0100

    Clean app entrypoint

--- lib/app.dart
+++ lib/app.dart
@@ -1,22 +1,14 @@
-import 'dart:developer';
-
 import 'package:flutter/material.dart';
-import 'package:keepscore/bloc/auth/auth_bloc.dart';
 
 // Pages
-import 'package:keepscore/pages/login_page.dart';
 import 'package:keepscore/pages/home_page.dart';
 import 'package:keepscore/pages/profile_page.dart';
 import 'package:keepscore/pages/splash_page.dart';
 
 class AppFrame extends StatelessWidget {
-  final GlobalKey<NavigatorState> _navigator = GlobalKey<NavigatorState>();
-
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    final bloc = AuthProvider.of(context);
-
     return MaterialApp(
       title: 'Keepscore',
       theme: ThemeData(
@@ -29,19 +21,6 @@ class AppFrame extends StatelessWidget {
       },
       onGenerateRoute: (_) => SplashPage.route(),
       home: HomePage(),
-      // home: StreamBuilder(
-      //   stream: bloc.status,
-      //   builder: (_, AsyncSnapshot<AuthStatus> snapshot) {
-      //     log('Data! ' + snapshot.data.toString());
-      //     switch (snapshot.data) {
-      //       case AuthStatus.authenticated:
-      //         return HomePage();
-      //       default:
-      //         log('Render login page!');
-      //         return P();
-      //     }
-      //   },
-      // ),
       debugShowCheckedModeBanner: false,
     );
   }
