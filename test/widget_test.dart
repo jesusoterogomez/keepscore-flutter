50298fdc9e09afaa5aac53f33359a1865380d2c0
Jesús Otero <jesusoterogomez@gmail.com>
2021-03-14 14:24:54 +0100

    Fix broken test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:keepscore/app.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(AppContainer());
+    await tester.pumpWidget(AppFrame());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
