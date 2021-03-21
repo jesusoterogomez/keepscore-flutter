335eb1de20a12358bc630030750b13228c059b71
Jesús Otero <jesusoterogomez@gmail.com>
2021-03-21 20:28:00 +0100

    Added info to match tile

--- lib/components/match_tile.dart
+++ lib/components/match_tile.dart
@@ -7,21 +7,71 @@ class MatchTile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    Team teamA = match.teams[0];
+    Team teamB = match.teams[1];
+
     return Container(
       padding: EdgeInsets.all(10),
       child: Center(
         child: Card(
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: <Widget>[
-              ListTile(
-                // leading: CircleAvatar(
-                //   backgroundImage: NetworkImage(plan.author.avatarUrl),
+          child: Container(
+            padding: EdgeInsets.all(20),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                Row(
+                  children: [
+                    Text(
+                      match.createdAt.toDate().toString(),
+                    )
+                  ],
+                ),
+                Row(
+                  children: [
+                    Text('[A] '),
+                    CircleAvatar(
+                      maxRadius: 12,
+                      backgroundImage: NetworkImage(teamA.attack.photoURL),
+                    ),
+                    Text(teamA.attack.firstName),
+                    Text(' [D] '),
+                    CircleAvatar(
+                      maxRadius: 12,
+                      backgroundImage: NetworkImage(teamA.defense.photoURL),
+                    ),
+                    Text(teamA.defense.firstName),
+                    Text(' [S] '),
+                    Text(teamA.score.toString()),
+                  ],
+                ),
+                Row(
+                  children: [
+                    Text('[A] '),
+                    CircleAvatar(
+                      maxRadius: 12,
+                      backgroundImage: NetworkImage(teamB.attack.photoURL),
+                    ),
+                    Text(teamB.attack.firstName),
+                    Text(' [D] '),
+                    CircleAvatar(
+                      maxRadius: 12,
+                      backgroundImage: NetworkImage(teamB.defense.photoURL),
+                    ),
+                    Text(teamB.defense.firstName),
+                    Text(' [S] '),
+                    Text(teamB.score.toString()),
+                  ],
+                ),
+
+                // ListTile(
+                //   // leading: CircleAvatar(
+                //   //   backgroundImage: NetworkImage(plan.author.avatarUrl),
+                //   // ),
+                //   title: Text(match.teams[0].attack.displayName),
+                //   subtitle: Text(match.teams[0].defense.displayName),
                 // ),
-                title: Text(match.teams[0].attack.displayName),
-                subtitle: Text(match.teams[0].defense.displayName),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
