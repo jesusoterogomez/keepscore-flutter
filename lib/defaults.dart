5b00941d48edef421f9f99cdb5e0ff50bb92204d
Jesús Otero <jesusoterogomez@gmail.com>
2021-04-05 23:00:25 +0200

    Fix indentation in defaults file

--- lib/defaults.dart
+++ lib/defaults.dart
@@ -9,6 +9,7 @@ final BORDER_RADIUS = BorderRadius.circular(6);
 
 final BASE_THEME = ThemeData(
   brightness: Brightness.light,
+  backgroundColor: Colors.white10,
   primaryColor: Colors.black,
   accentColor: Colors.black,
   fontFamily: GoogleFonts.montserrat().fontFamily,
@@ -17,29 +18,68 @@ final BASE_THEME = ThemeData(
 
 final TextTheme textTheme = TextTheme(
   headline1: GoogleFonts.montserrat(
-      fontSize: 97, fontWeight: FontWeight.w300, letterSpacing: -1.5),
+    fontSize: 97,
+    fontWeight: FontWeight.w300,
+    letterSpacing: -1.5,
+  ),
   headline2: GoogleFonts.montserrat(
-      fontSize: 61, fontWeight: FontWeight.w300, letterSpacing: -0.5),
-  headline3: GoogleFonts.montserrat(fontSize: 48, fontWeight: FontWeight.w400),
+    fontSize: 61,
+    fontWeight: FontWeight.w300,
+    letterSpacing: -0.5,
+  ),
+  headline3: GoogleFonts.montserrat(
+    fontSize: 48,
+    fontWeight: FontWeight.w400,
+  ),
   headline4: GoogleFonts.montserrat(
-      fontSize: 34, fontWeight: FontWeight.w400, letterSpacing: 0.25),
-  headline5: GoogleFonts.montserrat(fontSize: 24, fontWeight: FontWeight.w400),
+    fontSize: 34,
+    fontWeight: FontWeight.w400,
+    letterSpacing: 0.25,
+  ),
+  headline5: GoogleFonts.montserrat(
+    fontSize: 24,
+    fontWeight: FontWeight.w400,
+  ),
   headline6: GoogleFonts.montserrat(
-      fontSize: 20, fontWeight: FontWeight.w500, letterSpacing: 0.15),
+    fontSize: 20,
+    fontWeight: FontWeight.w500,
+    letterSpacing: 0.15,
+  ),
   subtitle1: GoogleFonts.montserrat(
-      fontSize: 16, fontWeight: FontWeight.w400, letterSpacing: 0.15),
+    fontSize: 16,
+    fontWeight: FontWeight.w400,
+    letterSpacing: 0.15,
+  ),
   subtitle2: GoogleFonts.montserrat(
-      fontSize: 14, fontWeight: FontWeight.w500, letterSpacing: 0.1),
+    fontSize: 14,
+    fontWeight: FontWeight.w500,
+    letterSpacing: 0.1,
+  ),
   bodyText1: GoogleFonts.montserrat(
-      fontSize: 16, fontWeight: FontWeight.w400, letterSpacing: 0.5),
+    fontSize: 16,
+    fontWeight: FontWeight.w400,
+    letterSpacing: 0.5,
+  ),
   bodyText2: GoogleFonts.montserrat(
-      fontSize: 14, fontWeight: FontWeight.w400, letterSpacing: 0.25),
+    fontSize: 14,
+    fontWeight: FontWeight.w400,
+    letterSpacing: 0.25,
+  ),
   button: GoogleFonts.montserrat(
-      fontSize: 14, fontWeight: FontWeight.w500, letterSpacing: 1.25),
+    fontSize: 14,
+    fontWeight: FontWeight.w500,
+    letterSpacing: 1.25,
+  ),
   caption: GoogleFonts.montserrat(
-      fontSize: 12, fontWeight: FontWeight.w400, letterSpacing: 0.4),
+    fontSize: 12,
+    fontWeight: FontWeight.w400,
+    letterSpacing: 0.4,
+  ),
   overline: GoogleFonts.montserrat(
-      fontSize: 10, fontWeight: FontWeight.w400, letterSpacing: 1.5),
+    fontSize: 10,
+    fontWeight: FontWeight.w400,
+    letterSpacing: 1.5,
+  ),
 );
 
 final GRAY_DEFAULT = Color.fromRGBO(220, 220, 220, 100);
